0a3eadcbddf7d16cc2e6fbf7ccc0fd744f3d064c
Mehmet Aytaç Aktan <130919543+darthvulpix444@users.noreply.github.com>
2024-03-21 03:15:01 +0300

    Bubble sort example added.

---
+++ week4/vector_bubble_sorting.m
@@ -0,0 +1,23 @@
+% Örnek kullanım
+array = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
+sortedArray = bubbleSort(array);
+disp(sortedArray);
+
+
+
+function sortedArray = bubbleSort(array)
+    n = length(array);
+    sortedArray = array;
+    
+    for i = 1:n-1
+        for j = 1:n-i
+            if sortedArray(j) > sortedArray(j+1)
+                % Swap elements if they are in the wrong order
+                temp = sortedArray(j);
+                sortedArray(j) = sortedArray(j+1);
+                sortedArray(j+1) = temp;
+            end
+        end
+    end
+end
+
