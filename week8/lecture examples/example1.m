45127205fd7694e3b6ddf72b7262333e6f9b21e6
Mehmet Aytaç Aktan <thebluevulpix@gmail.com>
2024-04-20 12:08:26 +0300

    Example fixed.

--- week8/lecture examples/example1.m
+++ week8/lecture examples/example1.m
@@ -1,5 +1,5 @@
 % Generate a time vector
-time_vector = (0:0.001:1);
+time_vector = (0:0.001:2);
 
 % Generate a wave signal
 frequency = 2;
@@ -7,9 +7,15 @@ amplitude = 1;
 signal_sin = amplitude * sin(frequency*time_vector);
 
 % Generate a square wave signal
-swsignal = 50;
-signal_square = square(frequency*time_vector,swsignal) ;
+% Square wave parameters
+amplitude = 1; % Amplitude
+frequency = 1; % Frequency (Hz)
+duration = 3; % Duration (seconds)
 
+% Generate square wave
+square_wave = amplitude * square(2*pi*frequency*time_vector); % Calculate the value of a square wave at the specified frequency: 2*pi*frequency*t
+
+ 
 % Generate a noisy signal
 noise_amplitude = 0.5;
 noise = noise_amplitude*rand(size(time_vector));
@@ -18,20 +24,21 @@ signal_noise = signal_sin + noise;
 % Plot sine wave
 subplot(3,1,1)
 plot(time_vector, signal_sin);
-title();
-xlabel();
-ylabel();
+title('Sinusoidal Signal');
+xlabel('Time (s)');
+ylabel('Amplitude');
 
-% Plot sine wave
+% Plot square wave with higher resolution
 subplot(3,1,2)
-plot(time_vector, signal_square);
-title();
-xlabel();
-ylabel();
+plot(time_vector, square_wave);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('Square Wave');
 
-% Plot sine wave
+
+% Plot noisy signal
 subplot(3,1,3)
 plot(time_vector, signal_noise);
-title();
-xlabel();
-ylabel();
+title('Noisy Signal');
+xlabel('Time (s)');
+ylabel('Amplitude');
